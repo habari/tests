a41d80002526819afc1a17d421eace336b4a06c4
Matthias Bauer <moeffju@contrib.habariproject.org>
2007-08-24 15:53:04 +0000

    * Changed InputFilter to keep text content of stripped tags. * Changed Pingback Plugin to use InputFilter. * Props freakerz.

--- test_inputfilter.php
+++ test_inputfilter.php
@@ -34,6 +34,8 @@ $tests= array(
 	),
 	'URL parsing' => array(
 		"InputFilter::parse_url( 'http://hey:there@moeffju.net:8137/foo/bar?baz=quux#blah' ) == array ( 'scheme' => 'http', 'host' => 'moeffju.net', 'port' => '8137', 'user' => 'hey', 'pass' => 'there', 'path' => '/foo/bar', 'query' => 'baz=quux', 'fragment' => 'blah', 'is_relative' => false, 'is_pseudo' => false, 'is_error' => false, 'pseudo_args' => '', )",
+		"InputFilter::parse_url( 'http:moeffju.net/blog/' ) == array ( 'scheme' => 'http', 'host' => 'moeffju.net', 'port' => '', 'user' => '', 'pass' => '', 'path' => '/blog/', 'query' => '', 'fragment' => '', 'is_relative' => false, 'is_pseudo' => false, 'is_error' => false, 'pseudo_args' => '', )",
+		"InputFilter::parse_url( 'moeffju.net/blog/' ) == array ( 'scheme' => 'http', 'host' => 'moeffju.net', 'port' => '', 'user' => '', 'pass' => '', 'path' => '/blog/', 'query' => '', 'fragment' => '', 'is_relative' => false, 'is_pseudo' => false, 'is_error' => false, 'pseudo_args' => '', )",
 		"InputFilter::parse_url( '/furanzen/bla' ) == array ( 'scheme' => '', 'host' => '', 'port' => '', 'user' => '', 'pass' => '', 'path' => '/furanzen/bla', 'query' => '', 'fragment' => '', 'is_relative' => true, 'is_pseudo' => false, 'is_error' => false, 'pseudo_args' => '', )",
 		"InputFilter::parse_url( '?bla=barbaz&foo' ) == array ( 'scheme' => '', 'host' => '', 'port' => '', 'user' => '', 'pass' => '', 'path' => '', 'query' => 'bla=barbaz&foo', 'fragment' => '', 'is_relative' => true, 'is_pseudo' => false, 'is_error' => false, 'pseudo_args' => '', )",
 		"InputFilter::parse_url( '#' ) == array ( 'scheme' => '', 'host' => '', 'port' => '', 'user' => '', 'pass' => '', 'path' => '', 'query' => '', 'fragment' => '', 'is_relative' => true, 'is_pseudo' => false, 'is_error' => false, 'pseudo_args' => '', )",
@@ -50,6 +52,7 @@ $tests= array(
 		"InputFilter::filter( '<p onClick=\"window.alert(\\'stole yer cookies!\\');\">Do not click here.</p>\n<script>alert(\"See this?\")</script>' ) == '<p>Do not click here.</p>\n'",
 		// http://ha.ckers.org/blog/20070124/stopping-xss-but-allowing-html-is-hard/
 		"InputFilter::filter( '<IMG src=\"http://ha.ckers.org/\" style\"=\"style=\"a/onerror=alert(String.fromCharCode(88,83,83))//\" &ampgt;`&gt' ) == ''",
+		"InputFilter::filter( '<b>Hello world</b>\n\nThis is a <test>test</test> post.\n\nHere's a first XSS attack. <<SCRIPT>alert(\'XSS\');//<</SCRIPT>\n\nHere's a second try at a <a href=\"#\">second link</a>.\n\nHere's a second XSS attack. <IMG SRC=\" &#14;  javascript:alert(\'XSS\');\">\n\nHere's a third link hopefully <a href=\"#\">it won\'t get removed</a>.\n\n<em>Thanks!</em>' ) == ''",
 	),
 );
 
