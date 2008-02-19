79abce431735a0c5c00203af9386e25c990a9a5d
ringmaster <ringmaster@653ae4dd-d31e-0410-96ef-6bf7bf53c507>
2008-02-19 04:30:24 +0000

    Added some unit testing capabilities. Create a symlink in the htdocs directory to the tests directory.  Include the bootstrap.php file in your test file.  Extend the UnitTestCase class with methods that start with "test_".  Use the assertion methods to track test results.  Execute run() on an instance of your object to initiate the test.  Load your test in the browser via the symlink. Note that this is rudimentary, and should be improved as our needs require.  Functions are based somewhat loosely on the SimpleTest API: http://www.lastcraft.com/simple_test.php
    git-svn-id: http://svn.habariproject.org/habari/trunk/tests@1361 653ae4dd-d31e-0410-96ef-6bf7bf53c507

---
+++ bootstrap.php
@@ -0,0 +1,76 @@
+<?php
+
+/**
+* Habari unit test bootstrap file
+*
+* How to use:
+* Step 1: Create a symlink to the tests directory within the htdocs directory
+* Step 2: Include this file at the beginning of a test
+**/
+
+define('HABARI_PATH', dirname( dirname( __FILE__ ) ) );
+define('UNIT_TEST', true);
+define('DEBUG', true);
+
+class UnitTestCase
+{
+	public $messages = array();
+	public $passes = 0;
+	public $fails = 0;
+
+	public function assert_true($value, $message = 'Assertion failed')
+	{
+		if($value !== true) {
+			$this->messages[] = array($message, debug_backtrace());
+			$this->fails++;
+		}
+		else {
+			$this->passes++;
+		}
+	}
+
+	public function assert_false($value, $message = 'Assertion failed')
+	{
+		if($value !== false) {
+			$this->messages[] = array($message, debug_backtrace());
+			$this->fails++;
+		}
+		else {
+			$this->passes++;
+		}
+	}
+
+	public function named_test_filter( $function_name )
+	{
+		return preg_match('%^test_%', $function_name);
+	}
+
+	public function run()
+	{
+		$methods = get_class_methods($this);
+		$methods = array_filter($methods, array($this, 'named_test_filter'));
+		$cases = 0;
+		echo '<h1>' . get_class($this) . '</h1>';
+
+		foreach($methods as $method) {
+			$this->messages = array();
+
+			echo '<h2>' . $method . '</h2>';
+
+			$this->$method();
+
+			foreach($this->messages as $message) {
+				echo '<div><em>Fail:</em> ' . $message[0] . '<br/>' . $message[1][0]['file'] . ':' . $message[1][0]['line'] . '</div>';
+			}
+
+			$cases++;
+		}
+
+
+		echo "<div>{$cases}/{$cases} complete.  {$this->fails} failed tests.  {$this->passes} passed tests.</div>";
+	}
+}
+
+include '../index.php';
+
+?>
