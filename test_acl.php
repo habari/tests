33fb1153ca1c32c80805bb1dcff293e145c5c434
Michael C. Harris <michael@twofishcreative.com>
2008-04-09 03:01:12 +0000

    Adding initial ACL tests. These currently fail in trunk, but pass in the ACL branch.

---
+++ test_acl.php
@@ -0,0 +1,44 @@
+<?php
+
+include 'bootstrap.php';
+
+/**
+ * class ACLTest
+ * This class tests aspects of the ACL system
+ *
+ **/
+class ACLTest extends UnitTestCase
+{
+
+	/**
+	 * function test_admin_access
+	 * Tests permission related aspects of the Posts class
+	 *
+	 **/
+	function test_admin_access()
+	{
+		// Create an a user and add them to the admin group
+    //(which has been granted admin priviliges in installhandler).
+		$admin= User::create('username=adminuser&email=test@example.com&password=test');
+		$admin->add_to_group( 'admin' );
+
+		$this->assert_true(
+			$admin->can( 'admin' ),
+			'Admin user does not have admin permission.'
+		);
+
+		$user= User::create('username=normaluser&email=test@example.com&password=test');
+		$this->assert_false(
+			$user->can( 'admin' ),
+			'Unpriviliged user has admin permission.'
+		);
+
+	}
+
+}
+
+$poststest = new ACLTest();
+$poststest->run();
+
+
+?>
