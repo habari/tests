b948565d1e5cf7ceb7e410e1881fcae65533b418
ringmaster <ringmaster@653ae4dd-d31e-0410-96ef-6bf7bf53c507>
2009-02-28 17:10:15 +0000

    Adding more current PHPUnit-based tests to trunk in an effort to phase out existing hodge-podge tests.  Instructions for using the test appear in the tests/AllTests.php file.  Old tests remain in the tests directory until they're all moved over.  Tests from the PHPUnit branch have been refactored into this code.
    I've also grabbed the autop unit test, but it's hanging my test runner, so it's commented out for now.  (Not sure if it was complete in the autop branch.)
    
    git-svn-id: http://svn.habariproject.org/habari/trunk/tests@3271 653ae4dd-d31e-0410-96ef-6bf7bf53c507

---
+++ system/classes/formatTest.php
@@ -0,0 +1,40 @@
+﻿<?php
+
+
+class system_classes_FormatTest extends PHPUnit_Framework_TestCase
+{
+
+	public function testSimpleAutop()
+	{
+		$this->assertEquals('<p>foo</p>', Format::autop('foo'));
+	}
+
+	/*
+	public function autopDataProvider()
+	{
+		$autopTestDataPath = dirname(__FILE__) . '/../data/autop';
+		$data = array();
+		for ($n=1; $n<=99; $n++) {
+			$suff = str_pad($n, 2, '0', STR_PAD_LEFT);
+			$inputFile = $autopTestDataPath . '/text' . $suff . '.input.txt';
+			$wantedFile = $autopTestDataPath . '/text' . $suff . '.wanted.txt';
+			echo $inputFile;
+			if (!is_readable($inputFile)) {
+				break; // no need to keep looping
+			}
+			$data[] = array(file_get_contents($inputFile), file_get_contents($wantedFile));
+		}
+		return $data;
+	}
+
+	/**
+	 * @dataProvider autopDataProvider
+	 */
+	/*
+	public function testAutop($in, $out)
+	{
+		$this->assertEquals($out, Format::autop($in));
+	}
+	*/
+}
+?>
