283d5055b835e87675dd17dc3fb4d7a39a6c2541
Sean Coates <sean@caedmon.net>
2009-03-14 20:46:01 +0000

    bring tests up to phpunit's normal operating procedure, maintain ringmaster's entry point for phpedit (I hope), and move old tests to old/

--- system/classes/formatTest.php
+++ system/classes/formatTest.php
@@ -1,5 +1,6 @@
 ﻿<?php
 
+require_once dirname( dirname( dirname( __FILE__ ) ) ) . DIRECTORY_SEPARATOR . 'phpunit_bootstrap.php';
 
 class system_classes_FormatTest extends PHPUnit_Framework_TestCase
 {
