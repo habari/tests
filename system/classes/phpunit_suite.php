01b91437bf9fc4dbcd1fd9430e81985c88dc8bf6
Sean Coates <sean@caedmon.net>
2010-01-21 03:43:33 +0000

    fix test suite (unset the DirectoryIterator so it doesn't get serialized, and declare the class before it's referenced)

--- system/classes/phpunit_suite.php
+++ system/classes/phpunit_suite.php
@@ -4,15 +4,6 @@
 require_once dirname( dirname( dirname( __FILE__ ) ) ) . DIRECTORY_SEPARATOR . 'phpunit_bootstrap.php';
 require_once 'PHPUnit/Framework.php';
 
-$testclasses = array();
-foreach (new DirectoryIterator( dirname( __FILE__ ) ) as $d) {
-	if ($d->isDot() || substr($d, -8) != 'Test.php') {
-		continue;
-	}
-	require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . $d;
-	Habari_system_classes_TestSuite::$testclasses[] = substr((string)$d, 0, strlen((string)$d) - 4); // trim .php
-}
-
 class Habari_system_classes_TestSuite extends PHPUnit_Framework_TestSuite
 {
 	public static $testclasses = array();
@@ -25,3 +16,14 @@ class Habari_system_classes_TestSuite extends PHPUnit_Framework_TestSuite
 		return $suite;
 	}
 }
+
+$testclasses = array();
+foreach (new DirectoryIterator( dirname( __FILE__ ) ) as $d) {
+	if ($d->isDot() || substr($d, -8) != 'Test.php') {
+		continue;
+	}
+	require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . $d;
+	Habari_system_classes_TestSuite::$testclasses[] = substr((string)$d, 0, strlen((string)$d) - 4); // trim .php
+}
+unset($d);
+
