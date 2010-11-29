2f8b6b6ec8cd1bf806f1faa14137e7ef1d36fec9
Rick Cockrum <rickcockrum@gmail.com>
2010-11-29 20:18:04 +0000

    Update the Tag tests to work properly with the current implementation of the Tag class.

--- system/classes/tagTest.php
+++ system/classes/tagTest.php
@@ -11,7 +11,7 @@ class system_classes_TagTest extends PHPUnit_Framework_TestCase
 	public function setup()
 	{
 		$this->slug = Utils::slugify( $this->text );
-		$this->tag = new Tag( array( 'tag_text' => $this->text, 'tag_slug' => $this->slug ) );
+		$this->tag = new Tag( array( 'term_display' => $this->text, 'term' => $this->slug ) );
 	}
 
 	public function teardown()
@@ -23,45 +23,41 @@ class system_classes_TagTest extends PHPUnit_Framework_TestCase
 	{
 		// Create a tag from a parameter array
 		$params = array(
-			'tag_slug' => $this->slug,
-			'tag_text' => $this->text
+			'term' => $this->slug,
+			'term_display' => $this->text,
 		);
 		$t = new Tag($params);
 
 		$this->assertType('Tag', $t, 'Result should be of type Tag');
-		$this->assertEquals($this->slug, $t->tag_slug, 'The slug should equal the slug value passed in.' );
-		$this->assertEquals($this->text, $t->tag_text, 'The text should equal the text value passed in.' );
+		$this->assertEquals($this->slug, $t->term, 'The slug should equal the slug value passed in.' );
+		$this->assertEquals($this->text, $t->term_display, 'The text should equal the text value passed in.' );
 
-		// Create a tag from a Term
-		$term = new Term($this->text);
-		$t = new Tag($term);
-
-		$this->assertType('Tag', $t, 'Result should be of type Tag');
-		$this->assertEquals($this->text, $t->tag_text, 'The text should equal the text value passed in.' );
 	}
 
 	public function test_create_tag()
 	{
-		$t = Tag::create( array( 'tag_text' => $this->text, 'tag_slug' => $this->slug ) );
+		$t = Tag::create( array( 'term_display' => $this->text, 'term' => $this->slug ) );
 		$this->assertType( 'Tag', $t );
 		// Check the tag's id is set.
 		$this->assertGreaterThan(0, (int)$t->id, 'The Tag id should be greater than zero');
-		$this->assertEquals($this->slug, $t->tag_slug, 'The slug should equal the slug value passed in.' );
-		$this->assertEquals($this->text, $t->tag_text, 'The text should equal the text value passed in.' );
-		$t->delete();
+		$this->assertEquals($this->slug, $t->term, 'The slug should equal the slug value passed in.' );
+		$this->assertEquals($this->text, $t->term_display, 'The text should equal the text value passed in.' );
+		$this->assertGreaterThan( 0, (int)$t->mptt_left , 'The Tag mptt_left should be greater than zero' );
+		$this->assertGreaterThan( 0, (int)$t->mptt_right , 'The Tag mptt_right should be greater than zero' );
+		Tags::vocabulary()->delete_term( $t );
 	}
 
 	public function test_insert_tag()
 	{
-		$count = count( Tags::get() );
-		$res = $this->tag->insert();
+		$count = Tags::vocabulary()->count_total();
+		$res = Tags::vocabulary()->add_term( $this->tag );
 		if ( $res ) {
 			$this->assertType( 'Tag', $res );
-			$this->assertEquals( $count + 1, count( Tags::get() ) );
-			$t = Tag::get( $this->text );
+			$this->assertEquals( $count + 1, Tags::vocabulary()->count_total() );
+			$t = Tags::vocabulary()->get_term( $this->text, 'Tag' );
 			$this->assertType( 'Tag', $t );
-			$this->assertEquals( $this->text, $t->tag_text );
-			$t->delete();
+			$this->assertEquals( $this->text, $t->term_display );
+			Tags::vocabulary()->delete_term( $t );
 		}
 		else {
 			$this->assertEquals( $res, FALSE );
@@ -71,34 +67,34 @@ class system_classes_TagTest extends PHPUnit_Framework_TestCase
 
 	public function test_update_tag()
 	{
-		$this->tag->insert();
-		$t = Tag::get( $this->tag->tag_text );
-		$t->tag_text = 'Updated Test Tag';
+		Tags::vocabulary()->add_term( $this->tag );
+		$t = Tags::vocabulary()->get_term( $this->tag->term_display, 'Tag' );
+		$t->term_display = 'Updated Test Tag';
 		$t->update();
-		$new_tag = Tag::get( $t->id );
-		$this->assertEquals( $new_tag->tag, $t->tag_text );
-		$t->delete();
+		$new_tag = Tags::vocabulary()->get_term( $t->id, 'Tag' );
+		$this->assertEquals( $new_tag->term_display, $t->term_display );
+		Tags::vocabulary()->delete_term( $t );
 	}
 
 	public function test_delete_tag()
 	{
-		$count = count( Tags::get() );
-		$t = $this->tag->insert();
-		$this->assertEquals( $count + 1, count( Tags::get() ) );
-		$t->delete();
-		$this->assertEquals( $count, count( Tags::get() ) );
+		$count = Tags::vocabulary()->count_total();
+		$t = Tags::vocabulary()->add_term( $this->tag );
+		$this->assertEquals( $count + 1, Tags::vocabulary()->count_total() );
+		Tags::vocabulary()->delete_term( $t );
+		$this->assertEquals( $count, Tags::vocabulary()->count_total() );
 	}
 
 	public function test_get_tag()
 	{
 		$this->tag->insert();
 		// Get tag by text
-		$t = Tag::get( $this->text );
-		$this->assertEquals( $t->tag_text, $this->tag->tag_text );
+		$t = Tags::vocabulary()->get_term( $this->text, 'Tag' );
+		$this->assertEquals( $t->term_display, $this->tag->term_display );
 		// Get tag by id
-		$t = Tag::get( $t->id );
-		$this->assertEquals( $t->tag_text, $this->tag->tag_text );
-		$t->delete();
+		$t = Tags::vocabulary()->get_term( (int)$t->id, 'Tag' );
+		$this->assertEquals( $t->term_display, $this->tag->term_display );
+		Tags::vocabulary()->delete_term( $t );
 	}
 
 	public function test_attach_to_post_tag()
