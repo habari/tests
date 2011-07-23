e3914bf8e8ed8e874e3898e06c012445ca7a1146
ringmaster <a_github@midnightcircus.com>
2011-07-23 12:36:17 -0400

    Allow tests to run form the command line.  Include HTML adapter.  Allow specifying habari path via command line, or search up the directory structure automatically.  Output buffering.

--- old/bootstrap.php
+++ old/bootstrap.php
@@ -8,8 +8,25 @@
 * Step 2: Include this file at the beginning of a test
 **/
 
+$shortopts = 'r::';
+$longopts = array();
+$options = getopt($shortopts, $longopts);
+
 if(!defined('HABARI_PATH')) {
-	define('HABARI_PATH', dirname( dirname( __FILE__ ) ) );
+	if(isset($options['r'])) {
+		define('HABARI_PATH', $options['r']);
+	}
+	else {
+		// Try traversing up until we find an index.php
+		$dirname = dirname(dirname( __FILE__ ));
+		while(!file_exists($dirname . '/index.php')) {
+			$dirname = dirname($dirname);
+			if(strlen($dirname) <= 1) {
+				throw new Exception("Couldn't find Habari's index.php");
+			}
+		}
+		define('HABARI_PATH', $dirname );
+	}
 }
 
 if(!defined('UNIT_TEST')) {
@@ -108,25 +125,24 @@ class UnitTestCase
 	{
 		if(isset($this->asserted_exception)) {
 			$this->fail_count++;
-			echo '<div><em>Fail:</em> ' . $this->asserted_exception[1] . '<br/>' . $this->asserted_exception[0] . '</div>';
+			$this->messages[] = array($this->asserted_exception[1] . ': ' . $this->asserted_exception[0]);
 		}
 		foreach($this->checks as $check => $message) {
 			$this->fail_count++;
-			echo '<div><em>Fail:</em> ' . $message . '</div>';
+			$this->messages[] = array($message);
 		}
 	}
 
-	public function run()
+	public function run($results)
 	{
 		$methods = get_class_methods($this);
 		$methods = array_filter($methods, array($this, 'named_test_filter'));
 		$cases = 0;
-		echo '<h1>' . get_class($this) . '</h1>';
+
+		$results->test(get_class($this));
 
 		foreach($methods as $method) {
 			$this->messages = array();
-
-			echo '<h2>' . $method . '</h2>';
 
 			$this->pre_test();
 			if(method_exists($this, 'setup')) {
@@ -134,10 +150,12 @@ class UnitTestCase
 			}
 
 			try {
+				ob_start();
 				$this->$method();
+				$output = ob_get_clean();
 			}
 			catch(Exception $e) {
-				if(strpos($e->getMessage(), $this->asserted_exception) !== false || get_class($e) == $this->asserted_exception[0]) {
+				if(strpos($e->getMessage(), $this->asserted_exception[0]) !== false || get_class($e) == $this->asserted_exception[0]) {
 					$this->pass_count++;
 					$this->asserted_exception = null;
 				}
@@ -149,8 +167,8 @@ class UnitTestCase
 						$ary = next($trace);
 					}
 					$ary = current($trace);
-					echo '<div><em>Exception '. get_class($e) .':</em> ' . $e->getMessage() . '<br/>' . $ary['file'] . ':' . $ary['line'] . '</div>';
-					echo '<pre>' . print_r($trace, 1) . '</pre>';
+//					echo '<div><em>Exception '. get_class($e) .':</em> ' . $e->getMessage() . '<br/>' . $ary['file'] . ':' . $ary['line'] . '</div>';
+//					echo '<pre>' . print_r($trace, 1) . '</pre>';
 				}
 			}
 
@@ -159,14 +177,14 @@ class UnitTestCase
 			}
 			$this->post_test();
 
-			foreach($this->messages as $message) {
-				echo '<div><em>Fail:</em> ' . $message[0] . '<br/>' . $message[1][0]['file'] . ':' . $message[1][0]['line'] . '</div>';
-			}
+			$results->method_results(get_class($this), $method, $this->messages);
 
 			$this->case_count++;
 		}
 
-		echo "<div class=\"test complete\">{$this->case_count}/{$this->case_count} tests complete.  {$this->fail_count} failed assertions.  {$this->pass_count} passed assertions.  {$this->exception_count} exceptions.</div>";
+		$results->summary(get_class($this), get_object_vars($this));
+
+		return $results;
 	}
 
 	public static function run_one($classname)
@@ -175,10 +193,9 @@ class UnitTestCase
 			return;
 		}
 		$testobj = new $classname();
-		echo '<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"></head><body>';
 
-		$testobj->run();
-		echo '</body></html>';
+		$testobj->run($results = new UnitTestResults());
+		echo $results;
 	}
 
 	public static function run_all()
@@ -192,12 +209,12 @@ class UnitTestCase
 		$classes = get_declared_classes();
 		$classes = array_unique($classes);
 		sort($classes);
-		echo '<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"></head><body>';
+		$results = new UnitTestResults();
 		foreach($classes as $class) {
 			$parents = class_parents($class, false);
 			if(in_array('UnitTestCase', $parents)) {
 				$obj = new $class();
-				$obj->run();
+				$obj->run($results);
 
 				$pass_count += $obj->pass_count;
 				$fail_count += $obj->fail_count;
@@ -205,8 +222,9 @@ class UnitTestCase
 				$case_count += $obj->case_count;
 			}
 		}
-		echo "<div class=\"all test complete\">{$case_count}/{$case_count} tests complete.  {$fail_count} failed assertions.  {$pass_count} passed assertions.  {$exception_count} exceptions.</div>";
-		echo '</body></html>';
+
+		echo $results;
+//		echo "<div class=\"all test complete\">{$case_count}/{$case_count} tests complete.  {$fail_count} failed assertions.  {$pass_count} passed assertions.  {$exception_count} exceptions.</div>";
 	}
 
 	public static function run_dir($directory = null)
@@ -223,7 +241,124 @@ class UnitTestCase
 	}
 }
 
-include '../index.php';
+class UnitTestResults
+{
+	private $methods = array();
+	private $tests = array();
+	private $summaries = array();
+	private $options = array();
+
+	function __construct()
+	{
+		global $options;
+		$this->options = $options;
+		$this->options['HABARI_PATH'] = HABARI_PATH;
+	}
+
+	function __toString()
+	{
+		if(defined('STDIN')) {
+			return $this->out_console();
+		}
+		else {
+			return $this->out_html();
+		}
+	}
+
+	function test($test)
+	{
+		$this->tests[] = $test;
+	}
+
+	function out_html()
+	{
+		if(count($this->tests) > 1) {
+			$title = "Test Results for " . count($this->tests) . " tests";
+		}
+		else {
+			$title = "Test Results for " . reset($this->tests);
+		}
+
+		$output = "<!DOCTYPE HTML><html><head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\"><title>{$title}</title></head><body>";
+		foreach($this->tests as $test) {
+			$output .= "<h1>{$test}</h1>";
+
+			foreach($this->methods[$test] as $methodname => $messages)
+			{
+				$output .= "<h2>{$methodname}</h2>";
+				foreach($messages as $message) {
+					$output .= '<div><em>Fail:</em> ' . $message[0];
+					if(count($message) > 1) {
+						$output .= '<br/>' . $message[1][0]['file'] . ':' . $message[1][0]['line'];
+					}
+					$output .= '</div>';
+				}
+			}
+
+			$summary = $this->summaries[$test];
+			$output .= "<div class=\"test complete\">{$summary['case_count']}/{$summary['case_count']} tests complete.  {$summary['fail_count']} failed assertions.  {$summary['pass_count']} passed assertions.  {$summary['exception_count']} exceptions.</div>";
+		}
+
+		$output .= '<footer><h3>Options</h3><table>';
+		foreach($this->options as $k => $v) {
+			$output .= "<tr><th>{$k}</th><td>{$v}</td></tr>";
+		}
+		$output .= '</table></footer>';
+
+		$output .= '</body></html>';
+
+		return $output;
+	}
+
+	function out_console()
+	{
+		if(count($this->tests) > 1) {
+			$title = "Test Results for " . count($this->tests) . " tests";
+		}
+		else {
+			$title = "Test Results for " . reset($this->tests);
+		}
+
+		$output = array();
+		$output[] = "==== {$title} ====";
+		foreach($this->tests as $test) {
+			$output[]= "\n=== {$test} ===";
+
+			foreach($this->methods[$test] as $methodname => $messages)
+			{
+				$output[]= "  {$methodname}";
+				foreach($messages as $message) {
+					$output[]= '    Fail: ' . $message[0];
+					if(count($message) > 1) {
+						$output[]= '      ' . $message[1][0]['file'] . ':' . $message[1][0]['line'];
+					}
+				}
+			}
+
+			$summary = $this->summaries[$test];
+			$output[]= "\n{$summary['case_count']}/{$summary['case_count']} tests complete.  {$summary['fail_count']} failed assertions.  {$summary['pass_count']} passed assertions.  {$summary['exception_count']} exceptions.";
+		}
+
+		$output[]= "\n=== Options ===";
+		foreach($this->options as $k => $v) {
+			$output[]= "  {$k}: {$v}";
+		}
+
+		return implode("\n", $output) . "\n";
+	}
+
+	function method_results($test, $method, $results)
+	{
+		$this->methods[$test][$method] = $results;
+	}
+
+	function summary($test, $values)
+	{
+		$this->summaries[$test] = $values;
+	}
+}
+
+include HABARI_PATH . '/index.php';
 
 endif;
 
