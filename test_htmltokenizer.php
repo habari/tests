29b7a1d98632b2e66bfb61770a181c79d4dbc213
rick-c <rickcockrum@gmail.com>
2011-09-08 15:11:47 -0400

    Get some useful information out of test_htmltokenizer.

--- test_htmltokenizer.php
+++ test_htmltokenizer.php
@@ -1,26 +1,32 @@
 <?php
-return;
 /**
  * Test for the HTMLTokenizer class.
  */
 include('bootstrap.php');
 
+class HTMLTokenizerTest extends UnitTestCase
+{
+	protected $html_strs = array();
+
+	function setup()
+	{
+
 // hixie's tasty tag soup -- http://ln.hixie.ch/?start=1137740632&count=1
-$html_strs[]= <<<_EOF_
+$this->html_strs[]= <<<_EOF_
 <!DOCTYPE HTML><title>Hello World</title><p title="example">Some text.</p><!-- A comment. -->
 _EOF_;
 
 // BigJibby's tag soup
-$html_strs[]= <<<_EOF_
+$this->html_strs[]= <<<_EOF_
 <p>I am <div><script src="ohnoes" /><a>not a paragraph.</a><p CLASS=old><span> Or am I?</span>
 _EOF_;
 
 // ha.ckers.org
-$html_strs[]= <<<_EOF_
+$this->html_strs[]= <<<_EOF_
 <IMG src="http://ha.ckers.org/" style"="style="a/onerror=alert(String.fromCharCode(88,83,83))//" &ampgt;`&gt
 _EOF_;
 
-$html_strs[]= <<<_EOF_
+$this->html_strs[]= <<<_EOF_
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"
 [ <!ATTLIST html habari CDATA #IMPLIED> ]
@@ -37,11 +43,11 @@ $html_strs[]= <<<_EOF_
 </html>
 _EOF_;
 
-$html_strs[]= <<<_EOF_
+$this->html_strs[]= <<<_EOF_
 <html><title>Oh &apos;eck!<body>This is a badly tag-soupy HTML document.</html>
 _EOF_;
 
-$html_strs[]= <<<_EOF_
+$this->html_strs[]= <<<_EOF_
 <html>
 <head><title>Hey</title></head>
 <body onLoad="window.alert('zomg.');">
@@ -50,11 +56,22 @@ $html_strs[]= <<<_EOF_
 </body>
 </html>
 _EOF_;
+	}
 
-foreach ($html_strs as $html_str) {
-	$t= new HTMLTokenizer( $html_str );
-	$tokens= $t->parse();
-	Utils::debug( $html_str, $tokens );
+	function test_tokenizer()
+	{
+		foreach ( $this->html_strs as $html_str ) {
+			$t = new HTMLTokenizer( $html_str );
+			$tokens = $t->parse();
+			$new_str = (string)$tokens;
+
+			$this->assert_identical( $html_str, $new_str, "<br>" . Utils::htmlspecialchars( $html_str ) . "<br>" . Utils::htmlspecialchars( $new_str ) );
+		}
+	}
+
+	function teardown()
+	{
+	}
 }
-
+HTMLTokenizerTest::run_one( 'HTMLTokenizerTest' );
 ?>
