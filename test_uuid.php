1f9ab56dd473cf65d374fb9467cde5a73b180a44
rick-c <rickcockrum@gmail.com>
2011-09-16 10:46:55 -0400

    Port test_uuid from the phpunit tests. None of it has been implemented.

---
+++ test_uuid.php
@@ -0,0 +1,34 @@
+<?php
+include 'bootstrap.php';
+
+class UUIDTest extends UnitTestCase
+{
+
+	public function test__tostring()
+	{
+		$this->mark_test_incomplete( 'This test has not been implemented yet.' );
+	}
+
+	public function test_get_array()
+	{
+		$this->mark_test_incomplete( 'This test has not been implemented yet.' );
+	}
+
+	public function test_get_raw()
+	{
+		$this->mark_test_incomplete( 'This test has not been implemented yet.' );
+	}
+
+	public function test_get_hex()
+	{
+		$this->mark_test_incomplete( 'This test has not been implemented yet.' );
+	}
+
+	public function test_get()
+	{
+		$this->mark_test_incomplete( 'This test has not been implemented yet.' );
+	}
+}
+
+UUIDTest::run_one( 'UUIDTest' );
+?>
