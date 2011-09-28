9d1d4b28f7e3e3729e69d28b42669df27f948a5d
mikelietz <codger+git@gmail.com>
2011-09-28 19:08:37 +0000

    Add incomplete tests for UserGroups and User classes.

--- test_user.php
+++ test_user.php
@@ -4,14 +4,144 @@ include 'bootstrap.php';
 
 class UserTest extends UnitTestCase
 {
-	function test_createuser()
+	private $user;
+
+	public function setup()
 	{
-		$user = User::create( array( 'username' => 'testcaseuser', 'email' => 'test@example.com', 'password' => 'test') );
+		$this->user = User::create( array( 'username' => 'testcaseuser', 'email' => 'test@example.com', 'password' => 'test') );
+
+	}
+
+	public function test_anonymoususer()
+	{
+		$this->mark_test_incomplete();
+	}
+
+	public function test_identifyuser()
+	{
+		$this->mark_test_incomplete();
+	}
+
+	public function test_createuser()
+	{
 		$this->assert_true(
-			$user instanceof User,
+			$this->user instanceof User,
 			'Could not create test user.'
 		);
-		$user->delete();
+
+		// @TODO: test action_user_insert_allow
+	}
+
+	public function test_updateuser()
+	{
+		$this->mark_test_incomplete();
+	}
+
+	public function test_deleteuser()
+	{
+		$this->mark_test_incomplete();
+
+		// @TODO: test action_user_delete_allow
+	}
+
+	public function test_rememberuser()
+	{
+		$this->mark_test_incomplete();
+	}
+
+	public function test_forgetuser()
+	{
+		$this->mark_test_incomplete();
+	}
+
+	public function test_authenticateuser()
+	{
+		$this->mark_test_incomplete();
+	}
+
+	public function test_getuser()
+	{
+		$this->mark_test_incomplete();
+	}
+
+	public function test_getuserid()
+	{
+		$this->mark_test_incomplete();
+	}
+
+	public function test_countuserposts()
+	{
+		$this->mark_test_incomplete();
+	}
+
+	public function test_commenteruser()
+	{
+		$this->mark_test_incomplete();
+	}
+
+	public function test_usercan()
+	{
+		$this->mark_test_incomplete();
+	}
+
+	public function test_usercanany()
+	{
+		$this->mark_test_incomplete();
+	}
+
+	public function test_usercannot()
+	{
+		$this->mark_test_incomplete();
+	}
+
+	public function test_grantuser()
+	{
+		$this->mark_test_incomplete();
+	}
+
+	public function test_denyuser()
+	{
+		$this->mark_test_incomplete();
+	}
+
+	public function test_revokeuser()
+	{
+		$this->mark_test_incomplete();
+	}
+
+	public function test_listusergroups()
+	{
+		$this->mark_test_incomplete();
+	}
+
+	public function test_useringroup()
+	{
+		$this->mark_test_incomplete();
+	}
+
+	public function test_addusertogroup()
+	{
+		$this->mark_test_incomplete();
+	}
+
+	public function test_removeuserfromgroup()
+	{
+		$this->mark_test_incomplete();
+	}
+
+	public function test_getuserinfo()
+	{
+		$this->mark_test_incomplete();
+	}
+
+	public function test_getuserurlargs()
+	{
+		$this->mark_test_incomplete();
+	}
+
+	public function teardown()
+	{
+		$this->user->delete();
 	}
 }
 
---
+++ test_usergroups.php
@@ -0,0 +1,30 @@
+<?php
+
+include 'bootstrap.php';
+
+class UserGroupsTest extends UnitTestCase
+{
+	public function setup()
+	{
+
+	}
+
+	public function test_getgroups()
+	{
+		$this->mark_test_incomplete();
+	}
+
+	public function test_getallgroups()
+	{
+		$this->mark_test_incomplete();
+	}
+
+	public function teardown()
+	{
+
+	}
+}
+
+UserGroupsTest::run_one('UserGroupsTest');
+
+?>
