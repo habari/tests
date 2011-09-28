44226ddd8ed30e35353589526a4947b7191d62a6
mikelietz <codger+git@gmail.com>
2011-09-28 19:13:06 +0000

    Add incomplete tests for the Users class.

---
+++ test_users.php
@@ -0,0 +1,35 @@
+<?php
+
+include 'bootstrap.php';
+
+class UsersTest extends UnitTestCase
+{
+	public function setup()
+	{
+
+	}
+
+	public function test_getusers()
+	{
+		$this->mark_test_incomplete();
+	}
+
+	public function test_getusersbyinfo()
+	{
+		$this->mark_test_incomplete();
+	}
+
+	public function test_getallusers()
+	{
+		$this->mark_test_incomplete();
+	}
+
+	public function teardown()
+	{
+
+	}
+}
+
+UsersTest::run_one('UsersTest');
+
+?>
