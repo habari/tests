2914f4a1b3b04e90e0e1fffa8c025f2619ab38b4
mikelietz <codger+git@gmail.com>
2011-11-28 17:54:50 +0000

    Display the message, shoved all the way to the right.

--- plugin_admin.php
+++ plugin_admin.php
@@ -28,7 +28,7 @@
 		<span class='pct30'><?php echo $result['name']?></span><span class='pct10'><?php echo $result['complete']?></span><span class='pct10'><?php echo $result['pass']?></span><span class='pct10'><?php echo $result['fail']?></span><span class='pct40'>&nbsp;</span>
 		<ul id='<?php echo $result['name']; ?>' class='methods'>
 		<?php foreach ( $result['methods'] as $method ): ?>
-			<li class='<?php echo $method['result']; ?>'><?php echo $method['name']?></li>
+			<li class='<?php echo $method['result']; ?>'>[<?php echo substr( $method['result'], 0, 1 ); ?>] <?php echo $method['name']?><span class="message"><?php echo isset( $method['message'] ) ? $method['message'] : ''; ?></span></li>
 		<?php endforeach; ?>
 		</ul>
 	</div>
