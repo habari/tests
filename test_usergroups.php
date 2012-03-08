30fa75db14adfdb938bd6f63e0f4798ce9265ad2
mikelietz <codger+git@gmail.com>
2012-03-08 14:35:59 +0000

    Test a surprising amount of usergroups.php with a few assertions. The limit + offset combination seems to fail at the moment. 'fetch_fn' needs documentation.

--- test_usergroups.php
+++ test_usergroups.php
@@ -11,7 +11,20 @@ class UserGroupsTest extends UnitTestCase
 
 	public function test_get()
 	{
+		// test 'fetch_fn'
 		$this->mark_test_incomplete();
+
+		// test 'count'
+		$this->assert_equal( count( UserGroups::get_all() ), UserGroups::get( array( 'count' => true ) ) );
+
+		// test 'limit'
+		$this->assert_equal( count( UserGroups::get( array( 'limit' => 1 ) ) ), 1 );
+
+		// test 'offset'
+		$this->assert_equal( count( UserGroups::get( array( 'limit' => 1, 'offset' => 1 ) ) ), 1 );
+		$this->assert_not_equal( UserGroups::get( array( 'limit' => 1, 'offset' => 1 ) ), UserGroups::get( array( 'limit' => 1 ) ), 1 );
+
+		// test 'where', including an 'id'...
 	}
 
 	public function test_get_all()
