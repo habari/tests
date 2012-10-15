f6693ea8230cf3cb94fad821725d8fe0e1644d5a
rick-c <rickcockrum@gmail.com>
2012-10-15 17:42:32 -0400

    Fix test_get_habariversion() to work with current versioning scheme. Comment out svn related methods until they're replace with git versions.

--- units/test_version.php
+++ units/test_version.php
@@ -14,13 +14,13 @@ class VersionTest extends UnitTestCase
 
 	function test_get_habariversion()
 	{
-		$this->assert_equal( Version::HABARI_VERSION, Version::get_habariversion() );
+		$this->assert_equal( Version::HABARI_MAJOR_MINOR . Version::HABARI_RELEASE, Version::get_habariversion() );
 	}
 
-	function test_is_devel()
-	{
-		$this->assert_equal( strpos(Version::HABARI_SVN_HEAD_URL, '/trunk/') !== false || strpos(Version::HABARI_SVN_HEAD_URL, '/branches/') !== false, Version::is_devel() );
-	}
+//	function test_is_devel()
+//	{
+//		$this->assert_equal( strpos(Version::HABARI_SVN_HEAD_URL, '/trunk/') !== false || strpos(Version::HABARI_SVN_HEAD_URL, '/branches/') !== false, Version::is_devel() );
+//	}
 
 	function test_save_dbversion()
 	{
@@ -37,23 +37,23 @@ class VersionTest extends UnitTestCase
 		$this->assert_equal( false, Version::requires_upgrade() );
 	}
 
-	function test_get_svn_revision()
-	{
+//	function test_get_svn_revision()
+//	{
 		/* the below is copied verbatim from version.php. This may not be optimal testing. */
-		$rev = 0;
+//		$rev = 0;
 		// Cheating!
-		$stash_file = HABARI_PATH . '/.svn/entries';
-		if(file_exists($stash_file)) {
-			$info = file_get_contents($stash_file);
-			$info = explode("\n", $info);
-			if(strpos($info[4], 'svn.habariproject.org/habari/') !== false) {
-				$rev = intval(trim($info[3]));
-			}
-		}
-		if($rev == 0) {
-			$rev = intval(preg_replace('/[^0-9]/', '', Version::HABARI_SVN_REV));
-		}
-		$this->assert_equal( $rev, Version::get_svn_revision() );
-	}
+//		$stash_file = HABARI_PATH . '/.svn/entries';
+//		if(file_exists($stash_file)) {
+//			$info = file_get_contents($stash_file);
+//			$info = explode("\n", $info);
+//			if(strpos($info[4], 'svn.habariproject.org/habari/') !== false) {
+//				$rev = intval(trim($info[3]));
+//			}
+//		}
+//		if($rev == 0) {
+//			$rev = intval(preg_replace('/[^0-9]/', '', Version::HABARI_SVN_REV));
+//		}
+//		$this->assert_equal( $rev, Version::get_svn_revision() );
+//	}
 }
 ?>
