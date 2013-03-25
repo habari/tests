cad2e4777cf88793389b3a43895851df30a759b5
ringmaster <epithet@gmail.com>
2013-03-25 12:42:48 -0400

    Update FormUI tests for master/388.

--- units/test_formui.php
+++ units/test_formui.php
@@ -26,91 +26,94 @@ class FormUITest extends UnitTestCase
 	function test_add_a_control1()
 	{
 		$form = new FormUI('test2');
-		$form->append('text', 'firstname', 'option:test__username', 'Firstname');
-		$form->append('submit', 'save', 'Save');
+		$form->append('label', 'label_for_firstname')->set_label('Firstname')->append('text', 'firstname', 'option:test__username');
+		$form->append('submit', 'save')->set_caption('Save');
 		$form->out();
 	}
 
 	function test_add_a_control2()
 	{
 		$form = new FormUI('test3');
-		$form->append(new FormControlText('firstname', 'option:test__username', 'Firstname') );
-		$form->append(new FormControlSubmit('save', 'Save') );
+		$form->append(FormControlLabel::wrap('Firstname', FormControlText::create('firstname', 'option:test__username')));
+		$form->append(FormControlSubmit::create('save')->set_caption('Save'));
 		$form->out();
 	}
 
 	function test_modify_a_control1()
 	{
 		$form = new FormUI('test4');
-		$form->append('text', 'firstname', 'option:test__username', 'Firstname');
-		$form->firstname->caption = 'First Name';
+		$form->append('label', 'label_for_firstname')->set_label('Firstname')->append('text', 'firstname', 'option:test__username');
+		$form->append('submit', 'save')->set_caption('Save');
 		$form->append(new FormControlSubmit('save', 'Save') );
+
+		$form->label_for_firstname->set_label('First Name');
 		$form->out();
 	}
 
 	function test_modify_a_control2()
 	{
 		$form = new FormUI('test5');
-		$firstname_control = $form->append('text', 'firstname', 'option:test__username', 'Firstname');
-		$firstname_control->caption = 'First Name';
-		$form->append(new FormControlSubmit('save', 'Save') );
+		$form->append(FormControlLabel::wrap('Firstname', FormControlText::create('firstname', 'option:test__username')));
+		$form->append(FormControlSubmit::create('save')->set_caption('Save'));
+
+		$form->label_for_firstname->set_label('First Name');
 		$form->out();
 	}
 
 	function test_initial_control_values()
 	{
 		$form = new FormUI('test6');
-		$form->append('text', 'firstname', 'null:unused', 'Firstname:');
+		$form->append('text', 'firstname');
 		$form->firstname->value = 'Bob';
-		$form->append('submit', 'save', 'Save');
+		$form->append('submit', 'save');
 		$form->out();
 	}
 
 	function test_initial_control_values2()
 	{
 		$form = new FormUI('test7');
-		$form->append('text', 'firstname', 'username', 'Firstname:');
+		$form->append('text', 'firstname');
 
 		$form->firstname->value = 'Alice';
 
 		if($form->firstname->value == 'Bob') {
-		  $form->append('text', 'lastname', 'lastname', 'Lastname:');
+		  $form->append('text', 'lastname');
 		}
 
-		$form->append('submit', 'save', 'Save');
+		$form->append('submit', 'save');
 		$form->out();
 	}
 
-	function test_form_post_processing1()
+	function test_form_post_processing_by_method()
 	{
 		$this->check('callback', 'Did not call callback.');
 
 		$form = new FormUI('test8');
-		$form->append('text', 'firstname', 'username', 'Firstname:');
-		$form->append('submit', 'save', 'Save');
+		$form->append('text', 'firstname');
+		$form->append('submit', 'save');
 		$form->on_success( array( $this, 'my_callback1' ), 'Bob' );
-		$form->out();
+		$form->simulate(array(), true);
+		$form->get();
 	}
 
 	function my_callback1( $form, $special_name )
 	{
 		$this->pass_check('callback');
-		// Perform normal save routines
-		$form->save();
 		// Display the form normally with the default confirmation message
 		return false;
 	}
 
-	function test_form_post_processing2()
+	function test_form_post_processing_by_filter()
 	{
 		Plugins::register(array($this, 'filter_my_callback'), 'filter', 'my_callback');
 
-		$this->check('callback', 'Did not call callback - submit the form to test.');
+		$this->check('callback', 'Did not call callback.');
 
 		$form = new FormUI('test9');
-		$form->append('text', 'firstname', 'username', 'Firstname:');
+		$form->append('text', 'firstname');
 		$form->append('submit', 'save', 'Save');
 		$form->on_success('my_callback', 'Bob');
+		$form->simulate(array(), true);
 		$form->out();
 	}
 
@@ -127,36 +130,33 @@ class FormUITest extends UnitTestCase
 	function test_using_form_values()
 	{
 		$form = $this->get_form1();
-		$this->assert_true($form->about == 'About my site');
+		$form->get();
+		$this->assert_true($form->about->value == Options::get('test__about'));
 	}
 
 	function get_form1()
 	{
 		$form = new FormUI('test10');
-		$form->append('text', 'about', 'test__about', 'About');
+		$form->append('text', 'about', 'test__about');
 		return $form;
 	}
 
 	function test_create_a_custom_control()
 	{
-		Plugins::register(array($this, 'filter_available_templates'), 'filter', 'available_templates');
+		$form = new FormUI('test10');
+
+		// filter_available_templates gets called too early in the chain to use here, so this instead:
+		$form->get_theme()->add_template('control.customz', 'This value gets replaced in filter_include_template_file()');
 		Plugins::register(array($this, 'filter_include_template_file'), 'filter', 'include_template_file');
 		$this->check('template', 'Template not loaded.');
 
-		$form = new FormUI('test10');
-		$form->append('custom', 'test', 'test__about', 'About');
+		$form->append('custom', 'test', 'test__about');
 		$form->out();
-	}
-
-	function filter_available_templates( $list )
-	{
-		$list = array_merge($list, array('my_special_control_template'));
-		return $list;
 	}
 
 	function filter_include_template_file($filename, $template)
 	{
-		if($template == 'my_special_control_template') {
+		if($template == 'control.customz') {
   		$this->pass_check('template');
   		return dirname(__FILE__) . '/data/formcontrol_custom2.php';
 		}
@@ -171,13 +171,12 @@ class FormUITest extends UnitTestCase
 
 }
 
-if(class_exists('FormUI')):
+if(class_exists('\Habari\FormUI')):
 	class FormControlCustom extends FormControl {
-		function get( $forvalidation = true ) {
-			// Get the theme object to use to render this control
-			$theme = $this->get_theme( $forvalidation );
+		function get( Theme $theme ) {
 			// Tell the theme what template to render for this control
-			return $theme->fetch( 'my_special_control_template' );
+			$this->set_template('control.customz');
+			return parent::get($theme);
 		}
 	}
 
